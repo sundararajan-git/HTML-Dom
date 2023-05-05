747690c6e535eee56177499e0380a84526474a98
sundararajan-git <sundararajanselvarasu@gmail.com>
2023-05-05 16:19:24 +0530

    bom

---
+++ BOM/main.js
@@ -0,0 +1,23 @@
+
+// BOM - Browser Object Model
+// # Screen Obj
+console.log(window.screen); // returns the visitor's screen info
+
+// # Location Obj
+// location.href = "https://google.com";
+
+// OR
+
+// window.location.assign("https://google.com");
+// window.location = "https://google.com";
+// window.location.href = "https://google.com";
+
+// # History Obj
+history.back(); //- same as clicking back in the browser
+history.forward(); //- same as clicking forward in the browser
+
+// # Navigator Obj
+console.log(navigator.appVersion);
+console.log(navigator.platform);
+console.log(navigator.webdriver);
+console.log(navigator.product); //  returns the product name of the browser engine
