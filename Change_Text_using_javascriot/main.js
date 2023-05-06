a38582a3a452682a6dc08003f8e8335a82dc60cf
sundararajan-git <sundararajanselvarasu@gmail.com>
2023-05-06 21:19:04 +0530

    change the text using js

---
+++ Change_Text_using_javascriot/main.js
@@ -0,0 +1,6 @@
+
+// changing heading use javascript
+document.body.children[0].children[0].innerText   = "Learn to HTML DOM 😁";
+
+// Find the text in  document in programically
+console.log(window.find("HTML DOM"));
